99cb240b0a3b2cb7f76759fac5ce6901b2cdee45
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-04-06 17:25:24 +0200

    init run

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -15,7 +15,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -31,7 +31,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            cache_id=uuid.uuid4(),
                            states=st_ct,
                            amenities=amenities,
--- web_dynamic/README.md
+++
@@ -1,1 +0,0 @@
-# web dynamic with flask
